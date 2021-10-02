05fbbe5f2137017296669a617343c9625ca25456
Matthew Berglund <bergitastrophy@gmail.com>
2021-10-02 08:21:38 +0200

    added html and js files

---
+++ main.js
@@ -0,0 +1,9 @@
+// [] It should remove all non-alphanumeric characters (punctuation, spaces and symbols)
+// [] It should change all characters to upper or lowercase
+// [] If the string passed into the function is a palindrome, it should return true, otherwise false
+
+// Example input: racecar, RaceCar, race CAR, 2A3*3a2, 2A3 3a2, 2_A3*3#A2
+
+function isPalindrome(string) {
+
+}
